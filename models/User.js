be31ab9bb51577d6018517c118760bded17f5df4
Brown-scripts <nikkibrowm297@gmail.com>
2025-02-02 10:45:05 +0000

    verify shops

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  phone: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['buyer', 'seller', 'admin'], default: 'buyer' },
+}, { timestamps: true });
+
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
+
