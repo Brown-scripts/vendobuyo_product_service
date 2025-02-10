8af44a7c710315cac6e925a68c118a680cf6de76
Brown-scripts <nikkibrowm297@gmail.com>
2025-02-10 09:12:43 +0000

    fixed

--- routes/product.js
+++ routes/product.js
@@ -13,7 +13,7 @@ router.get('/product/:id', authenticate, getProductById);
 router.put('/product/:id', authenticateSellerOrAdmin, updateProduct);
 router.delete('/product/:id', authenticateSellerOrAdmin, deleteProduct);
 router.get('/search',authenticate, searchProducts);
-router.get('/seller/:sellerId', authenticateSellerOrAdmin, getSellerShopsByAdmin);
+router.get('/seller/:sellerId', authenticateSellerOrAdmin, getSellerProductsByAdmin);
 
 
 module.exports = router;
